2d39f5e6a46dfc18c03476e0d9c75fc7b6157c46
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2024-10-24 02:16:13 +0330

    Update Image.jsx
    add title for Image component

--- src/contents/Image/Image.jsx
+++ src/contents/Image/Image.jsx
@@ -1,6 +1,6 @@
 import './Image.css'
 
-export default function Image({ children ,url, alt , as = 'image', height = '', width = '', thumbnailSize = '196', className = '', rounded = '', ...props }) {
+export default function Image({ children, url, alt, title, as = 'image', height = '', width = '', thumbnailSize = '196', className = '', rounded = '', ...props }) {
 
     const roundedClass = {
         none: 'rounded-none',
@@ -18,8 +18,9 @@ export default function Image({ children ,url, alt , as = 'image', height = '', 
                     {...props}
                     src={url}
                     className={` ${rounded && roundedClass}` + `${className && ` ` + className}`}
-                    style={{ height: `${height && `${height}px`}` ,  width: `${width && `${width}px`}`}}
+                    style={{ height: `${height && `${height}px`}`, width: `${width && `${width}px`}` }}
                     alt={alt}
+                    title={title}
                 />
             }
             {as === 'responsive' &&
@@ -28,8 +29,10 @@ export default function Image({ children ,url, alt , as = 'image', height = '', 
                     src={url}
                     height={height}
                     className={`img-fluid` + ` ${rounded && roundedClass}` + `${className && ` ` + className}`}
-                    style={{ height: `${height && `${height}px`}` , width: `${width && `${width}px`}`}}
-                    alt={alt} />
+                    style={{ height: `${height && `${height}px`}`, width: `${width && `${width}px`}` }}
+                    alt={alt}
+                    title={title}
+                />
             }
             {as === 'thumbnail' &&
                 <img
@@ -37,14 +40,17 @@ export default function Image({ children ,url, alt , as = 'image', height = '', 
                     src={url}
                     className={`img-thumbnail` + ` ${rounded && roundedClass}` + `${className && ` ` + className}`}
                     style={{ height: `${thumbnailSize}px`, width: `${thumbnailSize}px` }}
-                    alt={alt} />
+                    alt={alt}
+                    title={title}
+                />
             }
             {as === 'svg' &&
                 <div
                     {...props}
                     className={`img-svg` + ` ${rounded && roundedClass}` + `${className && ` ` + className}`}
-                    style={{ height: `${height}px` , minWidth: `${height}px`}}
+                    style={{ height: `${height}px`, minWidth: `${height}px` }}
                     alt={alt}
+                    title={title}
                 >
                     {children}
                 </div>
