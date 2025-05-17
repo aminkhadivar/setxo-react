fec576dff835d9b0c52bcc99fb09c7770c4bed45
Amin Khadivar <62219173+aminkhadivar@users.noreply.github.com>
2025-05-17 19:36:53 +0330

    Update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
-        "./src/**/*.{css}",
+        "./src/**/*.css",
     ],
     theme: {
         extend: {
